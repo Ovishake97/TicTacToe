7f61a9699649ad2e5adffb6153347690a0595813
Abhishek Kumar Sharma <theabhishek.aks@gmail.com>
2020-10-08 10:26:22 +0530

    [Abhishek] Added it to UC1 branch

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            char[] output= ticTacToeGame.CreateBaord();
+        }
+    }
+
+    public class TicTacToeGame
+    {
+        public static char[] CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++) {
+                board[i] = ' ';
+            }
+            return board;
+        }
+
+
+    }
+}
+
+
